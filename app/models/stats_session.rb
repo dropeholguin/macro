d55315a59c68ec50ac2d952aa785ac3927693e2a
Pedro Holguin <dropeholguin1994@gmail.com>
2017-05-26 08:47:23 -0500

    User can see the total number of sessions they have passed

--- app/models/stats_session.rb
+++ app/models/stats_session.rb
@@ -4,4 +4,6 @@ class StatsSession < ApplicationRecord
 	scope :peoples_session, -> (session_id) { where("session_id = ?", session_id) }
 	scope :stats_sessions, -> (session_id) { where("session_id = ? AND is_passed = ?", session_id, true) }
 	scope :stats_session, -> (session_id, user_id){ where("session_id = ? AND user_id = ?", session_id, user_id)}
+	scope :number_session_passed, -> (user_id) { where("user_id = ? AND is_passed = ?", user_id, true) }
+
 end 
